6dfd3142193a5c12733567dd9496af67603fcc92
Yury Dmitriev <anticmex2@gmail.com>
2020-10-15 22:33:33 +0300

    modifed1

--- app/views.py
+++ app/views.py
@@ -13,27 +13,33 @@ def index(request):
 
 
 def bus_stations(request):
-    current_page = 1
+
     bus_row = []
     with open('data-398-2018-08-30.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             bus_row.append(row)
         paginator = Paginator(bus_row, 10)
-        page_number = int(request.GET.get('page', 1))
-        page_obj = paginator.get_page(page_number)
+        current_page = int(request.GET.get('page', 1))
+
+        page_obj = paginator.get_page(current_page)
         data = page_obj.object_list
+        page_back = None
         if page_obj.has_next():
             np = page_obj.next_page_number()
-            params = request.META.get('PATH_INFO', None) + "?" + urllib.parse.urlencode({'page': np})
+            page_next = "?" + urllib.parse.urlencode({'page': np})
+            if current_page > 1:
+                pp = page_obj.previous_page_number()
+                page_back = "?" + urllib.parse.urlencode({'page': pp})
 
-        next_page_url = params
+        else:
+            page_next = None
+
+        next_page_url = page_next
         return render(request, 'index.html', context={
 
             'bus_stations': data,
             'current_page': current_page,
-            'prev_page_url': None,
+            'prev_page_url': page_back,
             'next_page_url': next_page_url,
         })
-
-
